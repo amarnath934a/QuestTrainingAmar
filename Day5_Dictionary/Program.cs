9b8ef5c21ae207a56eabc7c5c0d1857fece2e68d
amarnath934a <110110186+amarnath934a@users.noreply.github.com>
2024-10-07 18:51:03 +0530

    Day5_3

---
+++ Day5_Dictionary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new Dictionary<string, string>();
+            d.Add("First Name", "John");
+            d.Add("Last Name", "Smith");
+            Console.WriteLine(d["First Name"]);
+
+            //Update
+            d["First Name"] = "Jonny";
+            Console.WriteLine(d["First Name"]);
+
+            //Removal & Use var
+            d.Remove("Last Name");
+            Console.WriteLine("After Removal: ");
+            foreach (var item in d)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");  
+            }
+
+            //if else
+            if (d.ContainsKey("First Name")) 
+            {
+                Console.WriteLine("Exists");
+            }
+            else
+            {
+                Console.WriteLine("Not Exists");
+            }
+            Console.ReadLine();
+        }
+    }
+}
