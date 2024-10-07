4a911acc1a96cfc07e1734b21c90281204e7ee09
amarnath934a <110110186+amarnath934a@users.noreply.github.com>
2024-10-07 18:52:11 +0530

    Day5_4

---
+++ Day5_DictStudents/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictStudents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new Dictionary<string, List<int>>();
+
+            while (true)
+            {
+                Console.Write("Enter an ID or q to exit: ");
+                var option = Console.ReadLine();
+
+                if (option == "q")
+                {
+                    break;
+                }
+
+                var marks = new List<int>();
+                for (int i = 1; i <= 3; i++)
+                {
+                    Console.Write($"Enter mark {i}: ");
+                    var mark = int.Parse(Console.ReadLine());
+                    marks.Add(mark);
+                }
+
+                d.Add(option, marks);
+            }
+
+            foreach (var item in d)
+            {
+                Console.WriteLine($"{item.Key}: {string.Join(", ", item.Value)}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
