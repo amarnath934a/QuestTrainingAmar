1443b1c0ab8ca9e5bdb44d0c1abc61fe18c4f56f
amarnath934a <110110186+amarnath934a@users.noreply.github.com>
2024-10-07 18:55:57 +0530

    Day5_7

---
+++ Day5_StudentMarks2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentMarks2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> studentMarks = new List<List<int>>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter the number of subjects for student {i + 1}: ");
+                int subjectCount = int.Parse(Console.ReadLine());
+                List<int> marks = new List<int>();
+
+                for (int j = 0; j < subjectCount; j++)
+                {
+                    Console.Write($"Enter mark for Subject {j + 1}: ");
+                    marks.Add(int.Parse(Console.ReadLine()));
+                }
+                studentMarks.Add(marks);
+            }
+
+            Console.WriteLine("\nMarks of all students:");
+            foreach (var marks in studentMarks)
+            {
+                Console.WriteLine(string.Join(", ", marks));
+            }
+            Console.ReadLine();
+        }
+    }
+}
